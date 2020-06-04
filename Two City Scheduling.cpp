c2b3d27ec4c2a7d7b529b741146d9709f60b0c1f
Sparsh Gupta <54849696+SparshGupta75@users.noreply.github.com>
2020-06-04 11:52:50 +0530

    Two city scheduling

---
+++ Two City Scheduling.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        
+        vector<pair<int,int>> v;
+        int ans=0;
+        
+        int n = costs.size();
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({costs[i][0]-costs[i][1],i});
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<n/2;i++)
+        {
+            ans+=costs[v[i].second][0];
+        }
+        for(int i=n/2;i<n;i++)
+        {
+            ans+=costs[v[i].second][1];
+        }
+        return ans;
+    }
+};
