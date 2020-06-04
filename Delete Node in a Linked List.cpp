7b3386599e927b248f3c8ea97707a6253b2aa318
Sparsh Gupta <54849696+SparshGupta75@users.noreply.github.com>
2020-06-04 12:29:18 +0530

    Updated!

--- Delete Node in a Linked List.cpp
+++ Delete Node in a Linked List.cpp
@@ -5,3 +5,17 @@ public:
      *node = *node->next;
     }
 };
+
+// Another approach below if we are asked to free the memory allocated to that
+// particular node
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        ListNode *temp = node->next;
+        *node = *temp;
+        delete(temp);
+        
+    }
+};
